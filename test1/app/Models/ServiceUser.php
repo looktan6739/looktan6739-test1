c7f2aca0fe6d83b02047c2c6fb10cc987fce8c66
looktan6739 <looktan1608@gmail.com>
2024-08-23 20:26:29 +0700

    Update ServiceUser.php

--- test1/app/Models/ServiceUser.php
+++ test1/app/Models/ServiceUser.php
@@ -1,43 +1,17 @@
 <?php
 namespace App\Models;
 
-use Illuminate\Foundation\Auth\User as Authenticatable;
-use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
 
-class ServiceUser extends Authenticatable
+class ServiceUser extends Model
 {
-    use HasFactory;
-
     protected $table = 'service_user';
     protected $primaryKey = 'user_id';
     public $timestamps = false; // Disable default timestamps
 
     protected $fillable = [
-        'username',
-        'password',
-        'first_name',
-        'last_name',
-        'house_number',
-        'road',
-        'sub_district',
-        'district',
-        'province',
-        'postal_code',
-        'user_picture',
-        'user_phone',
-        'created_date',
-        'created_by',
-        'updated_date',
-        'updated_by',
+        'username', 'password', 'first_name', 'last_name', 'house_number',
+        'road', 'sub_district', 'district', 'province', 'postal_code',
+        'user_picture', 'user_phone', 'created_date', 'created_by', 'updated_date', 'updated_by'
     ];
-
-    protected $hidden = [
-        'password',
-    ];
-
-    // Optionally, if you want to customize authentication columns
-    public function getAuthPassword()
-    {
-        return $this->password;
-    }
 }
